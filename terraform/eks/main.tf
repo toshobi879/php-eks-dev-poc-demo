5edeb3983771603f087aaf90f2d06cafb750946b
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-01-30 00:38:17 +0530

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "billow-poc-terraform-state-prod"
+    bucket = "billow-terraform-state-prod"
     key    = "vpc/terraform.tfstate"
     region = "us-west-1"
   }
