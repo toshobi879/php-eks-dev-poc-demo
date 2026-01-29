dc1ad794d77426206166613fe1b12fe582d1167c
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-01-30 00:39:09 +0530

    Update backend.tf

--- terraform/vpc/backend.tf
+++ terraform/vpc/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "billow-poc-terraform-state-prod"
+    bucket         = "billow-terraform-state-prod"
     key            = "vpc/terraform.tfstate"
     region         = "us-west-1"
     dynamodb_table = "terraform-locks-prod"
