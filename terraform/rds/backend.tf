bb0bc32f42eae8d014b6caf5bafd8f26fdf18de1
toshobi879 <moshuaib@schnelltechnocraft.com>
2026-02-10 17:44:40 +0530

    Update backend.tf

--- terraform/rds/backend.tf
+++ terraform/rds/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "billow-terraform-state-prod"
     key            = "rds/terraform.tfstate"
-    region         = "us-west-1"
+    region         = "us-west-2"
     dynamodb_table = "terraform-locks-prod"
     encrypt        = true
   }
